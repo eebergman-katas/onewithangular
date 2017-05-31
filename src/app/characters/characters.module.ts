004dba4056169c189e899f6de6e713786f612b6c
Erin Bergman <ebergman@pillartechnology.com>
2017-05-31 14:03:08 -0400

    Add species

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -5,12 +5,14 @@ import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersListComponent } from './characters-list/characters-list.component';
 import { CharactersDetailComponent } from './characters-detail/characters-detail.component';
 import { CharactersComponent } from './characters.component';
+import { SpeciesListComponent } from './species-list/species-list.component';
+import { SpeciesDetailComponent } from './species-detail/species-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CharactersRoutingModule
   ],
-  declarations: [CharactersListComponent, CharactersDetailComponent, CharactersComponent]
+  declarations: [CharactersListComponent, CharactersDetailComponent, CharactersComponent, SpeciesListComponent, SpeciesDetailComponent]
 })
 export class CharactersModule { }
