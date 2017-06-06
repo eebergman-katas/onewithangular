342ad6b8d7e5efd258c68af51ac3a7e58b9ca6a0
Erin Bergman <ebergman@pillartechnology.com>
2017-06-06 11:45:58 -0400

    Remove species component

--- src/app/characters/characters-routing.module.ts
+++ src/app/characters/characters-routing.module.ts
@@ -4,18 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CharactersComponent } from 'app/characters/characters.component';
 import { CharactersListComponent } from 'app/characters/characters-list/characters-list.component';
 import { CharactersDetailComponent } from 'app/characters/characters-detail/characters-detail.component';
-import { SpeciesListComponent } from 'app/characters/species-list/species-list.component';
-import { SpeciesDetailComponent } from 'app/characters/species-detail/species-detail.component';
 
 const routes: Routes = [
     { path: '', component: CharactersComponent, children: [
 
             { path: '', component: CharactersListComponent, children: [
                     { path: '', component: CharactersDetailComponent }
-                ]
-            },
-            { path: 'species', component: SpeciesListComponent, children: [
-                    { path: '', component: SpeciesDetailComponent }
                 ]
             }
         ]
--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -8,8 +8,6 @@ import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersComponent } from './characters.component';
 import { CharactersListComponent } from './characters-list/characters-list.component';
 import { CharactersDetailComponent } from './characters-detail/characters-detail.component';
-import { SpeciesListComponent } from './species-list/species-list.component';
-import { SpeciesDetailComponent } from './species-detail/species-detail.component';
 
 @NgModule({
   imports: [
@@ -21,9 +19,7 @@ import { SpeciesDetailComponent } from './species-detail/species-detail.componen
   declarations: [
     CharactersComponent,
     CharactersListComponent,
-    CharactersDetailComponent,
-    SpeciesListComponent,
-    SpeciesDetailComponent
+    CharactersDetailComponent
   ],
 })
 export class CharactersModule { }
