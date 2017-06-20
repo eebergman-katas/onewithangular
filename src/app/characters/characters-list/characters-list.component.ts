fa2a673ebb17c215a7e5dbbff009c0f50fc73f2e
Erin Bergman <ebergman@pillartechnology.com>
2017-06-20 09:34:02 -0400

    char list really works!

--- src/app/characters/characters-list/characters-list.component.ts
+++ src/app/characters/characters-list/characters-list.component.ts
@@ -19,24 +19,10 @@ export class CharactersListComponent implements OnInit {
 
   ngOnInit() {
     this.getCharacters();
-
-    setTimeout(
-      console.log(this.chars), 1000);
   }
 
   private getCharacters() {
-    for (let i = 1; i < 10; i++) { // change to fetching page numbers
-      this.dataService.fetchSwapiResults(this.source, i)
-        .subscribe(char => this.chars.push(...char));
-    }
-    this.chars.map((data) => this.sortBy(this.chars, 'name'));
-  }
-
-  private sortBy(data: any[], property: string) {
-    return data.sort((a: any, b: any) => {
-      if (a[property] < b[property]) { return -1; };
-      if (a[property] > b[property]) { return 1; };
-      return 0;
-    });
+    this.dataService.fetchSwapiResults(this.source)
+      .subscribe(char => this.chars.push(...char));
   }
 }
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/concat';
+import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/switchMap';
 
 import { FilmCharacter } from 'app/core';
 import { ConfigService } from './config.service';
@@ -10,15 +11,35 @@ import { ConfigService } from './config.service';
 @Injectable()
 export class DataService {
 
-  constructor(private http: Http, private configService: ConfigService) {
+  constructor(private http: Http, private configService: ConfigService) { }
+
+  public fetchSwapiResults(source: string): Observable<any[]> {
+
+    return this.http
+      .get(`${this.configService.apiBase}${source}${this.configService.apiPage}1`)
+      .map(results => results.json())
+      .switchMap((json) => {
+        const pages = json.count % 10 === 0
+          ? json.count / 10
+          : (json.count / 10) + 1;
+
+        const observables = [];
+
+        for (let i = 1; i < pages; i++) {
+          observables.push(this.http
+            .get(`${this.configService.apiBase}${source}${this.configService.apiPage}${i}`));
+        }
+
+        return Observable
+          .forkJoin(observables)
+          .map(results => results.map((result: Response) => result.json().results))
+          .map(results => [].concat(...results))
+          .map(results => results.sort((a: any, b: any) => {
+            if (a.name < b.name) { return -1; };
+            if (a.name > b.name) { return 1; };
+            return 0;
+          }))
+          .do(array => console.log(array.length));
+      });
   }
-
-  public fetchSwapiResults(source: string, page: number): Observable<any[]> {
-    return this.http.get(`${this.configService.apiBase}${source}${this.configService.apiPage}${page}`)
-      .map((res: Response) => res.json().results);
-      // .map((data) => this.sortBy(data, 'name'));
-  }
-
- 
-
 }
