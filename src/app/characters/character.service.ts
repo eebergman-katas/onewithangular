cdcd6cd9279049e502ed43a97a9baf59851c636f
Erin Bergman <erin.e.bergman@gmail.com>
2017-07-19 20:47:22 -0400

    Update to Ng/CLI 1.2.x

--- src/app/characters/character.service.ts
+++ src/app/characters/character.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/forkJoin';
 
 import { environment } from '../../environments/environment';
 import { Character } from 'app/core';
